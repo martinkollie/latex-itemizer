a3d8a03a17949843ce43bf0d8ef52021e62cf443
Martin Kollie <martinkollie@gmail.com>
2019-03-17 17:47:04 +0000

    Updates

---
+++ precache-manifest.67fc6a76a0fc527b4b7ac046451906e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00b8fcca288f902f1b8",
+    "url": "itemizer/static/js/main.8dd7406c.chunk.js"
+  },
+  {
+    "revision": "cc750390f8d4b81d8199",
+    "url": "itemizer/static/js/runtime~main.ff6528bf.js"
+  },
+  {
+    "revision": "82386e3cc7ff629b0918",
+    "url": "itemizer/static/js/2.e3fea541.chunk.js"
+  },
+  {
+    "revision": "cbd8b5a8a02328308f76296212fe3d72",
+    "url": "itemizer/static/media/logo.cbd8b5a8.svg"
+  },
+  {
+    "revision": "e60b1d79c7235537b70b9f8c8e5be2fc",
+    "url": "itemizer/index.html"
+  }
+];
