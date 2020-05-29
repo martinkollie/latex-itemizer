78f282024f6cee25477bb6c53e339730da3214fd
Martin Kollie <martinkollie@gmail.com>
2020-05-29 02:29:52 +0100

    Updates

---
+++ precache-manifest.b6ad386302e3d110ecf8d1a1948bc9a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29498b6222947cd3345a8c9eacaf41a2",
+    "url": "/latex-itemizer/index.html"
+  },
+  {
+    "revision": "20605230004637f75a22",
+    "url": "/latex-itemizer/static/js/2.7bdf833a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/latex-itemizer/static/js/2.7bdf833a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610ee77301ce148ea7ed",
+    "url": "/latex-itemizer/static/js/main.a7ba4704.chunk.js"
+  },
+  {
+    "revision": "089cbd4bfaf14944965c",
+    "url": "/latex-itemizer/static/js/runtime-main.d1221f1e.js"
+  },
+  {
+    "revision": "cbd8b5a8a02328308f76296212fe3d72",
+    "url": "/latex-itemizer/static/media/logo.cbd8b5a8.svg"
+  }
+]);
